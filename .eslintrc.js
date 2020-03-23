aaeb6484cecf3545c502f99d495c124de148604d
Jacob Jenkins <jacob_jenkins@live.com>
2020-03-23 08:38:26 +0000

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		"plugin:vue/essential",
+		"eslint:recommended",
+		"@vue/typescript/recommended",
+		// "@vue/prettier",
+		"@vue/prettier/@typescript-eslint"
+	],
+	parserOptions: {
+		ecmaVersion: 2020
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		"indent": ["error", "tab"],
+		"quotes": ["error", "single", { "avoidEscape": true }],
+		"comma-dangle": ["error", "only-multiline"],
+	}
 };
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+	<div id="app">
+		<svmm-nav></svmm-nav>
+	</div>
 </template>
 
+<script lang="ts">
+// import { readdirSync } from 'fs';
+// import { remote } from 'electron';
+import NavComponent from '@/components/nav.component.vue';
+
+export default {
+	name: 'app',
+	components: {
+		svmmNav: NavComponent,
+	},
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+	box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+html, body {
+		box-sizing: border-box;
+		height: 100vh;
+		width: 100vw;
+		margin: 0;
+		padding: 0;
+}
+</style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss" scoped>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	background-image: url('assets/sv-background.png');
+	background-size: cover;
+	background-position: bottom center;
+	background-repeat: no-repeat;
+	width: 100%;
+	height: 100%;
 }
 </style>
